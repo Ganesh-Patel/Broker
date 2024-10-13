9492b931610f17abb4edc0136f789c60cada13f6
Ganesh-Patel-U <ganesh.oficial158@gmail.com>
2024-10-13 12:00:32 +0530

    initial commit Structure created

---
+++ Backend/server.js
@@ -0,0 +1,13 @@
+import express from 'express'
+import dotenv from 'dotenv/config.js'
+
+const app=express();
+const port=process.env.PORT || 3000;
+
+app.use('/',(req,res)=>{
+    res.send('Hello World');
+})
+
+app.listen(port,()=>{
+    console.log('server started at port ',port)
+})
